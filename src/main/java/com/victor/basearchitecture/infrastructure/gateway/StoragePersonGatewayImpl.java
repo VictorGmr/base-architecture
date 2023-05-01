733ba8fdcd0e25c82d2192c7d7f91d8b68e4f802
Victor Guimaraes <victor.gmr@hotmail.com>
2023-05-01 13:08:58 -0300

    feat: add lombok allargsconstructor to gateway impl

--- src/main/java/com/victor/basearchitecture/infrastructure/gateway/StoragePersonGatewayImpl.java
+++ src/main/java/com/victor/basearchitecture/infrastructure/gateway/StoragePersonGatewayImpl.java
@@ -4,19 +4,17 @@ import com.victor.basearchitecture.domain.gateway.StoragePersonGateway;
 import com.victor.basearchitecture.domain.model.Person;
 import com.victor.basearchitecture.infrastructure.entity.PersonEntity;
 import com.victor.basearchitecture.infrastructure.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
+@AllArgsConstructor
 public class StoragePersonGatewayImpl implements StoragePersonGateway {
 
     PersonRepository personRepository;
-
-    StoragePersonGatewayImpl(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     @Override
     public Person create(Person person) {
